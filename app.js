3ae6034266533c18420bad1ee8c8e4a6be6e3b5f
Rose M <97209503+Rosendo-Martinez@users.noreply.github.com>
2024-04-23 17:15:08 -0500

    Get basic router working

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+const posts = require('./router/posts');
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+
+app.use('/', posts)
 
 app.listen(PORT)
