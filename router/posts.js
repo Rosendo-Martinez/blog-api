1b37244d414f3721107d6987dc88fe6676605d20
Rose M <97209503+Rosendo-Martinez@users.noreply.github.com>
2024-04-23 18:34:22 -0500

    Set up route for /posts

--- router/posts.js
+++ router/posts.js
@@ -2,7 +2,23 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/posts', (req, res) => {
-    res.json({ msg: 'get posts'});
+    res.json(`GET /posts not implemented`)
+})
+
+router.post('/posts', (req, res) => {
+    res.json({ msg: 'POST /posts not implemented', body: req.body, user: req.user })
+})
+
+router.get('/posts/:id', (req, res) => {
+    res.json({ msg: 'GET /posts/:id not implemeted', params: req.params, user: req.user })
+})
+
+router.delete('/posts/:id', (req, res) => {
+    res.json({ msg: 'DELETE /posts/:id not implemented', params: req.params, user: req.user })
+})
+
+router.put('/posts/:id', (req, res) => {
+    res.json({ msg: 'PUT /posts/:id not implemented', params: req.params, user: req.user, body: req.body })
 })
 
 module.exports = router
