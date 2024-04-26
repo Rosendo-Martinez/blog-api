5c8beea21cf238cab92801658b65d9945670e615
Rose M <97209503+Rosendo-Martinez@users.noreply.github.com>
2024-04-26 18:14:36 -0500

    Imp. login route

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken')
+
 module.exports.register = function(req, res) {
     res.json({ msg: 'Register not implemented', params: req.params, user: req.user, body: req.body })
 }
@@ -11,5 +13,19 @@ module.exports.getAccount = function(req, res) {
 }
 
 module.exports.login = function(req, res) {
-    res.json({ msg: 'Login to account not implemented', params: req.params, user: req.user, body: req.body })
+    // This code is temporary till DB is constructed.
+    // Once DB is set up, this should all be changed to use the 
+    // DB instead of using a hardcoded user.
+    const USER = { username: 'genos', password: 'disciple', id: '1' } // temporary till DB is constructed
+    const SECRET = '12345' // temporary till env var is set up
+
+    if (req.body.username === USER.username && req.body.password === USER.password) {
+        const token = jwt.sign({ id: USER.id }, SECRET, { expiresIn: '5m' })
+        res.json({ 
+            msg: "Here is your authentication token. Make sure to send it with your request every time you need to access a protected API endpoint.", 
+            token: token
+        })
+    } else {
+        res.status(401).json({ msg: 'Wrong username and/or password.' })
+    }
 }
