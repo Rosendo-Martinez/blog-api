c425441f01e3297b1d17f203515f088b2d53effd
Rose M <97209503+Rosendo-Martinez@users.noreply.github.com>
2024-04-27 18:07:11 -0500

    Add fake db calls

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
 const passport = require('../passportjs')
+const User = require('../fakeDB')
 
 module.exports.register = function(req, res) {
     res.json({ msg: 'Register not implemented', params: req.params, user: req.user, body: req.body })
@@ -14,14 +15,10 @@ module.exports.getAccount = function(req, res) {
 }
 
 module.exports.login = function(req, res) {
-    // This code is temporary till DB is constructed.
-    // Once DB is set up, this should all be changed to use the 
-    // DB instead of using a hardcoded user.
-    const USER = { username: 'genos', password: 'disciple', id: '1' } // temporary till DB is constructed
-    const SECRET = '12345' // temporary till env var is set up
+    const user = User.find(req.body.username, req.body.password)
 
-    if (req.body.username === USER.username && req.body.password === USER.password) {
-        const token = jwt.sign({ id: USER.id }, process.env.JWT_SECRET, { expiresIn: '5m' })
+    if (user) {
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '5m' })
         res.json({ 
             msg: "Here is your authentication token. Make sure to send it with your request every time you need to access a protected API endpoint.", 
             token: token
--- passportjs.js
+++ passportjs.js
@@ -1,18 +1,21 @@
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
 const passport = require('passport')
-
-const SECRET = '12345' // temporary till env var is set up
-const USERS = [{ username: 'genos', password: 'disciple', id: '1' }] // temporary till DB is constructed
+const User = require('./fakeDB')
 
 passport.use(new JwtStrategy(
     {
         jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
         secretOrKey: process.env.JWT_SECRET
-    }, 
+    },
     function verify(jwt_payload, done) {
-        const user = USERS.find((u) => u.id === jwt_payload.id)
-        return done(null, user)
+        const user = User.findById(jwt_payload.userId)
+
+        if (user) {
+            return done(null, user)
+        } else {
+            return done(null, false)
+        }
     }
 ))
 
