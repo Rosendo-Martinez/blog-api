c75a7b95280cf87092a4e461bc118a07cda8c6e0
Rose M <97209503+Rosendo-Martinez@users.noreply.github.com>
2024-04-29 20:39:28 -0500

    Factor out fake DB from strategy

--- passportjs.js
+++ passportjs.js
@@ -1,7 +1,7 @@
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
 const passport = require('passport')
-const User = require('./fakeDB')
+const User = require('./models/User')
 
 passport.use(new JwtStrategy(
     {
@@ -9,13 +9,15 @@ passport.use(new JwtStrategy(
         secretOrKey: process.env.JWT_SECRET
     },
     function verify(jwt_payload, done) {
-        const user = User.findById(jwt_payload.userId)
+        User.findById(jwt_payload.userId).exec()
+            .then(user => {
+                if (!user) {
+                    return done(null, false)
+                }
 
-        if (user) {
-            return done(null, user)
-        } else {
-            return done(null, false)
-        }
+                done(null, user)
+            })
+            .catch(err => done(err))
     }
 ))
 
