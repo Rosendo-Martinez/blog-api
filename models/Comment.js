45808b5bb5d144929ccf1250658db3c580666843
Rose M <97209503+Rosendo-Martinez@users.noreply.github.com>
2024-04-29 19:19:52 -0500

    Combine Comment & Reply Models

--- models/Comment.js
+++ models/Comment.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose')
 const CommentSchema = new mongoose.Schema({
     author: { type: mongoose.ObjectId, ref: 'User', required: true },
     text: { type: String, required: true, minLength: 1, trim: true },
-    date: { type: Date, default: new Date() },
-    post: { type: mongoose.ObjectId, ref: 'Post', required: true }
+    postedDate: { type: Date, default: new Date() },
+    commentOn: { type: mongoose.ObjectId, required: true, enum: ['Post', 'Comment'] },
+    commentOnType: { type: String, required: true, enum: ['Post', 'Comment'] }
 })
 
 module.exports = mongoose.model('Comment', CommentSchema)
