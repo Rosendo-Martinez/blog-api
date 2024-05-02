72c425aac0345c7502b42ce8a3874f2bc5e1c7ce
Rose M <97209503+Rosendo-Martinez@users.noreply.github.com>
2024-05-01 19:43:34 -0500

    Add Select False Option To hashedPassword

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt')
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, index: true, unique: true, minLength: 1, maxLength: 30, trim: true },
     email: { type: String, required: true, index: true, unique: true, minLength: 1, trim: true },
-    hashedPassword: { type: String, required: true, minLength: 1 }
+    hashedPassword: { type: String, required: true, minLength: 1, select: false }
 });
 
 /**
