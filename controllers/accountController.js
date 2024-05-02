f75f575842d288ea92ba07f53dd82cfefd1e87b6
Rose M <97209503+Rosendo-Martinez@users.noreply.github.com>
2024-05-01 20:06:27 -0500

    Add documentation to generateUserToken

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -5,6 +5,16 @@ const Author = require('../models/Author')
 const { body, validationResult } = require('express-validator')
 const bcrypt = require('bcrypt')
 
+/**
+ * Generates a JWT token for a user.
+ * 
+ * This function takes a user's ID and uses it to generate a JSON Web Token (JWT).
+ * The generated token can be used for secure communication between the client and the server
+ * by ensuring that each request from the client is authenticated.
+ * 
+ * @param {string} userId - The user's unique identifier.
+ * @returns {string} A JWT token that can be used to authenticate user requests.
+ */
 function generateUserToken(userId) {
     return jwt.sign({ userId: userId }, process.env.JWT_SECRET, { expiresIn: '5m' })
 }
